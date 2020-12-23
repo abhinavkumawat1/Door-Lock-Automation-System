0cee00af9555e96cc4498bce01dd574310081380
abhinavkumawat1 <54698732+abhinavkumawat1@users.noreply.github.com>
2020-12-23 12:26:07 +0530

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -1,14 +1,33 @@
-# Creating database   
+# Creating database 
+# It captures images and stores them in datasets  
+# folder under the folder name of sub_data 
 import cv2, sys, numpy, os 
-haar_file = 'haarcascade_frontalface_default.xml' 
+haar_file = 'haarcascade_frontalface_default.xml'
+  
+# All the faces data will be 
+#  present this folder 
 datasets = 'datasets'  
-sub_data = 'Abhinav Kumawat'       
+  
+  
+# These are sub data sets of folder,  
+# for my faces I've used my name you can  
+# change the label here 
+sub_data = 'Abhinav_Kumawat'     
+  
 path = os.path.join(datasets,sub_data) 
 if not os.path.isdir(path): 
     os.mkdir(path) 
+  
+# defining the size of images  
 (width, height) = (130, 100)     
+  
+#'0' is used for my webcam,  
+# if you've any other camera 
+#  attached use '1' like this 
 face_cascade = cv2.CascadeClassifier(haar_file) 
-webcam = cv2.VideoCapture(0)    
+webcam = cv2.VideoCapture(0)  
+  
+# The program loops until it has 30 images of the face. 
 count = 1
 while count < 30:  
     (_, im) = webcam.read() 
@@ -19,7 +38,8 @@ while count < 30:
         face = gray[y:y + h, x:x + w] 
         face_resize = cv2.resize(face, (width, height)) 
         cv2.imwrite('% s/% s.png' % (path, count), face_resize) 
-    count += 1   
+    count += 1
+      
     cv2.imshow('OpenCV', im) 
     key = cv2.waitKey(10) 
     if key == 27: 
