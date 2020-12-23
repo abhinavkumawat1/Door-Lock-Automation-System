01453d4be5f67f2c997981a7c5bf64c02878d24a
abhinavkumawat1 <54698732+abhinavkumawat1@users.noreply.github.com>
2020-12-23 12:23:16 +0530

    Update face_recognize.py

--- face_recognize.py
+++ face_recognize.py
@@ -1,46 +1,46 @@
-# It helps in identifying the faces 
+# Creating database 
+# It captures images and stores them in datasets  
+# folder under the folder name of sub_data 
 import cv2, sys, numpy, os 
-size = 4
 haar_file = 'haarcascade_frontalface_default.xml'
+  
+# All the faces data will be 
+#  present this folder 
 datasets = 'datasets'  
-# Part 1 
-print('Recognizing Face Please Be in sufficient Lights...') 
-(images, lables, names, id) = ([], [], {}, 0) 
-for (subdirs, dirs, files) in os.walk(datasets): 
-    for subdir in dirs: 
-        names[id] = subdir 
-        subjectpath = os.path.join(datasets, subdir) 
-        for filename in os.listdir(subjectpath): 
-            path = subjectpath + '/' + filename 
-            lable = id
-            images.append(cv2.imread(path, 0)) 
-            lables.append(int(lable)) 
-        id += 1
-        (width, height) = (130, 100) 
-(images, lables) = [numpy.array(lis) for lis in [images, lables]] 
-model = cv2.face.LBPHFaceRecognizer_create() 
-model.train(images, lables) 
-# Part 2
+  
+  
+# These are sub data sets of folder,  
+# for my faces I've used my name you can  
+# change the label here 
+sub_data = 'Abhinav_Kumawat'     
+  
+path = os.path.join(datasets,sub_data) 
+if not os.path.isdir(path): 
+    os.mkdir(path) 
+  
+# defining the size of images  
+(width, height) = (130, 100)     
+  
+#'0' is used for my webcam,  
+# if you've any other camera 
+#  attached use '1' like this 
 face_cascade = cv2.CascadeClassifier(haar_file) 
-webcam = cv2.VideoCapture(0) 
-while True: 
+webcam = cv2.VideoCapture(0)  
+  
+# The program loops until it has 30 images of the face. 
+count = 1
+while count < 30:  
     (_, im) = webcam.read() 
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4) 
     for (x, y, w, h) in faces: 
         cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2) 
         face = gray[y:y + h, x:x + w] 
         face_resize = cv2.resize(face, (width, height)) 
-        prediction = model.predict(face_resize) 
-        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 3) 
-        if prediction[1]<500: 
-           cv2.putText(im, '% s - %.0f' % 
-(names[prediction[0]], prediction[1]), (x-10, y-10),  
-cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0)) 
-        else: 
-          cv2.putText(im, 'not recognized',  
-(x-10, y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0)) 
-    cv2.imshow('OpenCV', im)       
+        cv2.imwrite('% s/% s.png' % (path, count), face_resize) 
+    count += 1
+      
+    cv2.imshow('OpenCV', im) 
     key = cv2.waitKey(10) 
     if key == 27: 
-          break
+        break
